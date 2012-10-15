04d5c5c097b363d80f8a94f561a3cb7c4f749309
Stephen Muss <stephenmuss@gmail.com>
2012-10-15 18:02:15 +1100

    Made some further changes to passbook creation tool

--- passbook/views.py
+++ passbook/views.py
@@ -21,6 +21,7 @@ def add_pass(request):
     if request.method == 'POST':
         """"""
     return render(request, 'passbook/add_pass.html', {'pass_types': Pass.PASS_TYPES,
+                                                      'transit_types': Pass.TRANSIT_TYPE_CHOICES,
                                                       'suggested_serial_number': uuid.uuid4().get_hex(),
                                                       'suggested_auth_token': uuid.uuid4().get_hex(),
                                                       'barcode_types': Barcode.FORMAT_CHOICES})
