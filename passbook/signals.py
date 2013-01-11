3ee4a6faa98b050c74f3030243c6a4df5cce7ce7
tim hsu <tim.yellow@gmail.com>
2013-01-11 17:22:02 +0800

    ignore dabase error as first time

--- passbook/signals.py
+++ passbook/signals.py
@@ -1,12 +1,15 @@
 from .models import Field, Pass, Location, Barcode, Signer
 from .update import Channels
 from django.db.models.signals import post_save
+from django.db.utils import DatabaseError
 import logging
 
 logger = logging.getLogger('passbook')
-
-channels = Channels(Signer.objects.all())
-
+try:
+    channels = Channels(Signer.objects.all())
+except DatabaseError as e:
+    logger.warn('signer table not available, it is safe if you see the message as first start up ')
+    
 def updatePassDate(_pass):
     logger.debug('updated related pass')
     _pass.description = _pass.description  # For Update Time, I change to auto update the field updated_at
