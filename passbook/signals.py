77bb7089300d38650a5b6e94e0f11c4290d326b1
tim hsu <tim.yellow@gmail.com>
2013-01-15 18:16:47 +0800

    fix circular import

--- passbook/signals.py
+++ passbook/signals.py
@@ -1,8 +1,8 @@
 from django.db.models.signals import post_save
 from django.db.utils import DatabaseError
-from .models import Field, Pass, Location, Barcode
 import logging
 import update
+from django.conf import settings
 from django.dispatch.dispatcher import Signal
 
 logger = logging.getLogger('passbook')
@@ -15,7 +15,6 @@ def notify_device(sender, identifier, token, **kwarg):
     channel = update.getChannels()
     channel.notify(identifier,token)
 
-from django.conf import settings
 
 
 if not getattr(settings, 'IN_UNIT_TEST',False):
